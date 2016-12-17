b789cc2c72c4929488e947467090a325d174a903
Hamze-Issa <hamze.j.i@gmail.com>
2016-12-17 20:53:58 +0200

    Delete CTestTestfile.cmake

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/juan/cognitive/gr-COWN/python
-# Build directory: /home/juan/cognitive/gr-COWN/python
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
